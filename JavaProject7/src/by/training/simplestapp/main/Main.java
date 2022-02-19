6e10acdd4a4551d1ae9494adf899ad9580ef853b
Max <99347438+Maximt76@users.noreply.github.com>
2022-02-19 18:35:24 +0300

    Task 03

---
+++ JavaProject7/src/by/training/simplestapp/main/Main.java
@@ -0,0 +1,13 @@
+package by.training.simplestapp.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int a = 5, b = 6;
+		
+		System.out.println("Периметр:" + 2 * (a + b));
+		System.out.println("Площадь:" + a * b);
+	}
+
+}
